9df37073d0f3c9f789554ca2b7bb89745caad9a9
Ambreen Hasan <ambodumbo@gmail.com>
2015-02-09 13:59:45 -0800

    added basic logic to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    # create a new user
-    #if the height and weight dimensions already exist, display male or female
-    # if not, create a new record and at random display male or female and save to the database
-    # add this logic to model
+    @user = User.new(user_params)
+
+    if @user.save && @user.exists?(height: params[:height], weight: params[:weight])
+      @user.where(:height: params[:height], weight: params[:weight]).first
+      render
+    elsif @user.save && !@user.exists?(height: params[:height], weight: params[:weight])
+      gender = ["Male", "Female"]
+      @user.create(height: params[:height], weight: params[:weight], gender: gender.sample)
+    end
+    # redirect_to root_path
   end
 
   private
 
   def user_params
+    params.require(:user).permit(:height, :weight)
   end
 end

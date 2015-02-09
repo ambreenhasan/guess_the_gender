c95fd9717bbc6e153c1341e00d1f2ac0f1b7d2b7
Ambreen Hasan <ambodumbo@gmail.com>
2015-02-09 12:32:03 -0800

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  validates :height, presence: true
+  validates :weight, presence: true
+
+  validates :height, numericality: true
+  validates :weight, numericality: true
+
+  validates :height, inclusion: {in: 48..96}
+  validates :weight, inclusion: {in: 50..700}
 end
